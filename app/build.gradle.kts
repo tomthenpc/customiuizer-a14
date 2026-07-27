186eb38611100722c217a379d549378f106e73b0
tomthenpc <oktelevision771@gmail.com>
2026-07-27 16:45:30 +0800

    build: bump version to r14.13.3 (181)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ if (hasReleaseSigning) {
     throw GradleException("Release signing is unavailable: ../keystore.properties was not found.")
 }
 
-val lastVersion = 178
-val lastVersionName = "r14.13.0"
+val lastVersion = 181
+val lastVersionName = "r14.13.3"
 val supportedLocales = setOf(
     "ru-rRU",
     "zh-rCN",
