f5e0024ca3e2ec076af684261f39328abac903bd
tomthenpc <oktelevision771@gmail.com>
2026-08-07 21:32:45 +0800

    build(a14): pin Gradle daemon to Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import org.gradle.buildconfiguration.tasks.UpdateDaemonJvm
+
 plugins {
     alias(libs.plugins.android.application) apply false
+}
+
+tasks.named<UpdateDaemonJvm>("updateDaemonJvm") {
+    toolchainDownloadUrls.empty()
 }
 
 tasks.register<Delete>("clean") {
